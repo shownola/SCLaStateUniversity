61654f4f0ad25c65e6e8c0185409c81f2c264812
shownola <swasieko@gmail.com>
2019-10-22 17:09:17 -0700

    Implement course model

---
+++ app/models/course.rb
@@ -0,0 +1,7 @@
+class Course < ApplicationRecord
+  validates :name, :short_name, presence: true, length: {minimum: 5, maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 300}
+
+
+
+end
