ebdc5cb465973007abb90f63465bb56b23366724
shownola <swasieko@gmail.com>
2019-10-23 09:06:13 -0700

    Implement student model with validations

---
+++ app/models/student.rb
@@ -0,0 +1,11 @@
+class Student < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 250},
+              uniqueness: {case_sensitive: false},
+              format: {with: VALID_EMAIL_REGEX}
+
+
+
+end
