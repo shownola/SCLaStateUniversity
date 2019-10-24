95d6ef66c406358165602e1921c9c3170645cbfd
shownola <swasieko@gmail.com>
2019-10-24 10:59:49 -0700

    Implement new student functionality and error notices with styling

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: {minimum: 2, maximum: 50}
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 250},
               uniqueness: {case_sensitive: false},
